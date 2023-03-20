ddd5f549623e9323782b87014ec3a622598bac72
Sarthak Shukla <sarthakshukla500@gmail.com>
2023-03-20 20:39:05 +0530

    eleventh commit

---
+++ lib/quiz/quizhomeScreen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:liberty_furies/quiz/quizpage.dart';
+
+
+
+class QuizHomeScreen extends StatefulWidget {
+  const QuizHomeScreen({super.key});
+
+  @override
+  State<QuizHomeScreen> createState() => _QuizHomeScreenState();
+}
+
+class _QuizHomeScreenState extends State<QuizHomeScreen> {
+  final _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset(
+              "assets/images/background.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          SafeArea(
+              child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(flex: 2),
+                Text("Lets play quiz",
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                Text(
+                  'Enter your information below',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Spacer(),
+                TextField(
+                  controller: _controller,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.5),
+                    decoration: InputDecoration(
+                        hintText: "Full Name",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        filled: true,
+                        fillColor: Colors.white54,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))))),
+                Spacer(), 
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => quizpage(name: _controller.text,)
+                           // QuizScreen(name:_controller.text.toString()
+                            ));
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10), // 15
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.blue,
+                        Colors.green,
+                      ]),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Text(
+                      "Lets Start Quiz",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Spacer(flex: 2),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/quiz/quizpage.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:liberty_furies/quiz/result.dart';
+
+
+import 'color.dart';
+import 'data/questionlist.dart';
+
+class quizpage extends StatefulWidget {
+  final name;
+  const quizpage({Key? key,required this.name}) : super(key: key);
+
+  @override
+  State<quizpage> createState() => _quizpageState();
+}
+
+class _quizpageState extends State<quizpage> {
+  int question_pos = 0;
+  int score = 0;
+  bool btnPressed = false;
+  PageController? _controller;
+  String btnText = "Next Question";
+  bool answered = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = PageController(initialPage: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.pripmaryColor,
+      body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: PageView.builder(
+            controller: _controller!,
+            onPageChanged: (page) {
+              if (page == questions.length - 1) {
+                setState(() {
+                  btnText = "See Results";
+                });
+              }
+              setState(() {
+                answered = false;
+              });
+            },
+            physics: new NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text( "Welcome to the Quick Quiz\n${widget.name}",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 31,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      "Question ${index + 1}/${questions.length}",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 200.0,
+                    child: Text(
+                      "${questions[index].question}",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                  for (int i = 0; i < questions[index].answers!.length; i++)
+                    Container(
+                      width: double.infinity,
+                      height: 50.0,
+                      margin: EdgeInsets.only(
+                          bottom: 20.0, left: 12.0, right: 12.0),
+                      child: RawMaterialButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        fillColor: btnPressed
+                            ? questions[index].answers!.values.toList()[i]
+                            ? Colors.green
+                            : Colors.red
+                            : AppColor.secondaryColor,
+                        onPressed: !answered
+                            ? () {
+                          if (questions[index]
+                              .answers!
+                              .values
+                              .toList()[i]) {
+                            score++;
+                            print("yes");
+                          } else {
+                            print("no");
+                          }
+                          setState(() {
+                            btnPressed = true;
+                            answered = true;
+                          });
+                        }
+                            : null,
+                        child: Text(questions[index].answers!.keys.toList()[i],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                            )),
+                      ),
+                    ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {
+                      if (_controller!.page?.toInt() == questions.length - 1) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ResultScreen(score)));
+                      } else {
+                        _controller!.nextPage(
+                            duration: Duration(milliseconds: 250),
+                            curve: Curves.easeInExpo);
+
+                        setState(() {
+                          btnPressed = false;
+                        });
+                      }
+                    },
+                    shape: StadiumBorder(),
+                    fillColor: Colors.blue,
+                    padding: EdgeInsets.all(18.0),
+                    elevation: 0.0,
+                    child: Text(
+                      btnText,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              );
+            },
+            itemCount: questions.length,
+          )),
+    );
+  }
+}
